5954711415153685798c9701b626180fe0adf20e
gustavopardini28 <gustavoemp28@gmail.com>
2022-11-03 22:31:42 -0300

    Added a new function where the total adapts according to the balance, when negative = red, positive = green,a responsive layout was also increased

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -7,20 +7,29 @@ export const SummaryContainer = styled.section`
   padding: 0 1.5rem;
 
   display: grid;
-  grid-template-columns: repeat(3, 1fr); 
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr)); 
   gap: 2rem;
 
   margin-top: -5rem; 
+
+  @media (max-width:952px ) {    
+    grid-template-columns: repeat(3,1fr);
+    overflow-x:auto;
+    
+    ::-webkit-scrollbar{
+      width: 0;
+    }
+  }
 `;
 
 interface SummaryCardProps{
-  variant?: 'green';
+  variant?: 'green' | 'red';
 }
 
 export const SummaryCard = styled.div<SummaryCardProps>`
   background: ${props => props.theme['gray-600']};
   border-radius: 6px;
-  padding: 2rem;
+  padding: 2rem 2rem 2rem 2rem;
 
   header{
     display: flex;
@@ -30,12 +39,19 @@ export const SummaryCard = styled.div<SummaryCardProps>`
   }
   
   strong{
-    display: block;
+    display: inline-block;
     margin-top:1rem;
     font-size:2rem;
+ 
   }
 
-  ${props => props.variant === 'green' && css`
-    background: ${props.theme['green-700']}
+  ${props => props.variant === 'red' && css`
+    background: ${props => props.theme['green-700']};
   `}
+
+  ${props => props.variant === 'green' && css`
+    background: ${props => props.theme['red-700']};
+  `}
+
+  
 `
