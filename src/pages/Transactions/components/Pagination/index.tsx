353ace4fbd0a125ae4f84dc045931939a00326ed
gustavopardini28 <gustavoemp28@gmail.com>
2022-11-07 22:58:57 -0300

    added limit for transactions in aplication

--- src/pages/Transactions/components/Pagination/index.tsx
+++ src/pages/Transactions/components/Pagination/index.tsx
@@ -1,4 +1,7 @@
 import { ButtonsContainer, PaginationButton } from "./styles";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 interface PaginationProps {
   totalPosts: number;
@@ -8,30 +11,39 @@ interface PaginationProps {
 }
 
 export function Pagination ({totalPosts, postsPerPage,setCurrentPage,currentPage}: PaginationProps) {
-    let pages = [];
-
+  const notify = () => toast('maximum post reached');
+    
+  
+  let pages = [];
     for( let i = 1; i<= Math.ceil(totalPosts/postsPerPage); i++) {
       pages.push(i)
+      console.log(pages.length)
+      if(pages.length > 3) {
+        
+         pages.splice(-1)
+         notify()
     }
-
+  }
+    
     return (
-     <ButtonsContainer>
-     {
-        pages.map((page,index) => {
-          
-        return (
-          
-        <PaginationButton 
-          key={index} 
-          variant={page == currentPage ? 'active' : ''} 
-          onClick={() => setCurrentPage(page)} 
-        > 
-          {page} 
-        </PaginationButton>     
-        )
-      })
-     }
-     </ButtonsContainer>
+      <>
+      <ButtonsContainer>
+        {
+          pages.map((page, index) => {
+            return (
+              <PaginationButton
+                key={index}
+                variant={page == currentPage ? 'active' : ''}
+                onClick={() => setCurrentPage(page)}
+              >
+                {page}
+              </PaginationButton>
+            )
+          })
+        }
+      </ButtonsContainer>
+      <ToastContainer/>
+      </>
     )
-}
+  }
 
